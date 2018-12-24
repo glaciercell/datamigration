389890e06606ae9c2c31a419cee69847ec3be3e3
Sachin Sharma <sachin1sharma@gmail.com>
2018-12-23 20:16:52 -0500

    merged into develop
    Signed-off-by: Sachin Sharma <sachin1sharma@gmail.com>

--- controllers/Migrator.cs
+++ controllers/Migrator.cs
@@ -13,7 +13,7 @@ namespace datamigration
 
     [Route("api/Migrator")]
     public class Migrator : Controller
-    {   
+    {
         private IDocumentClient _documentClient;
         private IConfiguration _configuration;
 
@@ -26,11 +26,25 @@ namespace datamigration
         [HttpGet("Migrate")]
         public void Migrate(IEnumerable<string> documentcollection)
         {
+            List<DocumentCollection> colls = ReadSource();
+
+            var migratecolls = colls.Where(z => documentcollection.Contains(z.Id, StringComparer.InvariantCultureIgnoreCase));
+            Parse(migratecolls);
+
+            WritetoTarget();
+        }
+
+        private List<DocumentCollection> ReadSource()
+        {
             var database = _configuration.GetSection("documentdb").GetSection("source").GetSection("database").Value;
             var db = _documentClient.CreateDatabaseQuery().ToList().FirstOrDefault(x => x.Id == database);
             var colls = _documentClient.CreateDocumentCollectionQuery(db.CollectionsLink).ToList();
-            var migratecolls = colls.Where(z  => documentcollection.Contains(z.Id, StringComparer.InvariantCultureIgnoreCase));
-            Parse(migratecolls);
+            return colls;
+        }
+
+        private void WritetoTarget()
+        {
+            throw new NotImplementedException();
         }
 
         private void Parse(IEnumerable<DocumentCollection> migratecolls)
